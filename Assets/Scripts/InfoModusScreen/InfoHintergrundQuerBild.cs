14fca0848ab7e280fd156475069bedcafbd7c6bf
Vichichi <aichhorn.vicky@gmail.com>
2021-01-28 15:20:07 +0100

    InformationenNeuDesigned. Yeet.

---
+++ Assets/Scripts/InfoModusScreen/InfoHintergrundQuerBild.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoHintergrundQuerBild : MonoBehaviour
+{
+    public GameObject Background;
+    public GameObject BackgroundOpen;
+    public GameObject Textfeld;
+    public GameObject ButtonOpen;
+    public GameObject ButtonClose;
+
+    public void OpenBackground()
+    {
+        BackgroundOpen.SetActive(true);
+        Background.SetActive(false);
+        Textfeld.SetActive(false);
+        ButtonOpen.SetActive(false);
+        ButtonClose.SetActive(true);
+    }
+
+    public void CloseBackground()
+    {
+        BackgroundOpen.SetActive(false);
+        Background.SetActive(true);
+        Textfeld.SetActive(true);
+        ButtonOpen.SetActive(true);
+        ButtonClose.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/InfoModusScreen/InfoHintergrundSwitch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoHintergrundSwitch : MonoBehaviour
+{
+    public GameObject Textfeld;
+    public GameObject ButtonOpen;
+    public GameObject ButtonClose;
+
+    public void OpenBackground()
+    {
+        Textfeld.SetActive(false);
+        ButtonOpen.SetActive(false);
+        ButtonClose.SetActive(true);
+    }
+
+    public void CloseBackground()
+    {
+        Textfeld.SetActive(true);
+        ButtonOpen.SetActive(true);
+        ButtonClose.SetActive(false);
+    }
+
+}
