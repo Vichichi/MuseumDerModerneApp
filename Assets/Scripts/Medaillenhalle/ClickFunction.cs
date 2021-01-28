e5cebe61531c7eee883a805c8f2ba78156a0994f
Vichichi <aichhorn.vicky@gmail.com>
2021-01-28 18:24:55 +0100

    Design implementierung und versucht, die medaillenhalle zu reparieren. Lösung muss noch gesucht werden

--- Assets/Scripts/Medaillenhalle/ClickFunction.cs
+++ Assets/Scripts/Medaillenhalle/ClickFunction.cs
@@ -6,39 +6,131 @@ using UnityEngine.UI;
 
 public class ClickFunction : MonoBehaviour
 {
+    //I hol ma de Ui-Images
     public Image popUpReminder;
+    public Image popUpReminder2;
+    public Image popUpReminder3;
+    public Image popUpReminder4;
+    public Image popUpReminder5;
+    public Image popUpReminder6;
+    public Image popUpReminder7;
+
     private RectTransform popUpReminderTransform;
-    Collider med1col;
-    public float smoothFactor = 5;
-    
+    private RectTransform popUpReminderTransform2;
+    private RectTransform popUpReminderTransform3;
+    private RectTransform popUpReminderTransform4;
+    private RectTransform popUpReminderTransform5;
+    private RectTransform popUpReminderTransform6;
+    private RectTransform popUpReminderTransform7;
+
+    Vector3[] Ohne1;
+
+
+    //------------------------------------------------------------------------------------------------------------//   
     // Start is called before the first frame update
     void Start()
     {
-        //Get the GameObjects Collider
-        med1col = GetComponent<Collider>();
-        //Stelle an Trigger auf False
-        med1col.isTrigger = false;
         //Get Image Components
         popUpReminderTransform = popUpReminder.GetComponent<RectTransform>();
-        //SAG WAS
-        Debug.Log("ES GEHT WUWU Trigger is: " +med1col.isTrigger);
+        popUpReminderTransform2 = popUpReminder2.GetComponent<RectTransform>();
+        popUpReminderTransform3 = popUpReminder3.GetComponent<RectTransform>();
+        popUpReminderTransform4 = popUpReminder3.GetComponent<RectTransform>();
+        popUpReminderTransform5 = popUpReminder3.GetComponent<RectTransform>();
+        popUpReminderTransform6 = popUpReminder3.GetComponent<RectTransform>();
+        popUpReminderTransform7 = popUpReminder3.GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(med1col.isTrigger == true)
+        if(Input.GetMouseButton(0))
         {
-            popUpReminderTransform.anchoredPosition = new Vector3(0.38516f, -860.7f, 0f);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
+            if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Gurken")
+            {
+                popUpReminderTransform.anchoredPosition = new Vector3(0.38516f, -815.93f, 0f);
+
+                popUpReminderTransform2.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform3.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform4.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform5.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform6.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform7.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+            }
+            
+            if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Bestiarium")
+            {
+                popUpReminderTransform2.anchoredPosition = new Vector3(0.38516f, -815.93f, 0f);
+
+                popUpReminderTransform.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform3.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform4.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform5.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform6.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform7.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+            }
+           
+            if (Physics.Raycast(ray, out hit) && hit.transform.tag == "AEIOU")
+            {
+                Debug.Log("AEIOU");
+                popUpReminderTransform3.anchoredPosition = new Vector3(0.38516f, -815.93f, 0f);
+
+                popUpReminderTransform.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform2.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform4.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform5.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform6.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform7.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+            }
+
+              if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Zungenbaerte")
+            {
+                popUpReminderTransform4.anchoredPosition = new Vector3(0.38516f, -815.93f, 0f);
+
+                popUpReminderTransform.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform2.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform3.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform5.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform6.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform7.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+            }
+
+              if (Physics.Raycast(ray, out hit) && hit.transform.tag == "EingangMdM")
+            {
+                popUpReminderTransform5.anchoredPosition = new Vector3(0.38516f, -815.93f, 0f);
+
+                popUpReminderTransform.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform2.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform3.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform4.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform6.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform7.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+            }
+
+              if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Drugs")
+            {
+                popUpReminderTransform6.anchoredPosition = new Vector3(0.38516f, -815.93f, 0f);
+
+                popUpReminderTransform.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform2.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform3.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform4.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform5.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform7.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+            }
+
+              if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Haus")
+            {
+                popUpReminderTransform7.anchoredPosition = new Vector3(0.38516f, -815.93f, 0f);
+
+                popUpReminderTransform.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform2.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform3.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform4.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform5.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+                popUpReminderTransform6.anchoredPosition = new Vector3(0.38516f, -1103.399f, 0f);
+            }
         }
-    }
-
-    void OnMouseDown()
-    {
-        //ANGEKLICKT
-        med1col.isTrigger = true;
-        //SAGS
-        Debug.Log("HALLO ES GEHT JETZT IST ES: " +med1col.isTrigger);
     }
 }
