cc49888596961d78107feaa3b21ea6922860d5d1
Vichichi <aichhorn.vicky@gmail.com>
2021-02-11 21:38:07 +0100

    Design von Maxima umgesetzt, yolo

---
+++ Assets/Scripts/Startscreen/scroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scroll : MonoBehaviour
+{
+	public GameObject Infomodus;
+	public GameObject Spielemodus;
+
+   public void Informationsmodus()
+   {
+		  Infomodus.SetActive(true);
+		  Spielemodus.SetActive(false);
+   }
+
+   public void Spieli()
+   {
+		  Spielemodus.SetActive(true);
+		  Infomodus.SetActive(false);
+   }
+}
